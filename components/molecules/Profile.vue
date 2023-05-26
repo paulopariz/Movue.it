a9aec99aa1f990bc0e0844940d0d74943b9f598c
paulopariz <paulopariz01@gmail.com>
2023-05-26 18:06:27 -0300

    component Profile

---
+++ components/molecules/Profile.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex items-center">
+    <img
+      src="https://cdn-icons-png.flaticon.com/512/266/266033.png"
+      alt="User"
+      class="mt-6 w-20 h-20 rounded-full"
+    />
+
+    <div class="flex flex-col w-full">
+      <strong class="text-title text-2xl">User</strong>
+      <p class="flex items-center">
+        <img src="icons/level.svg" alt="Level" class="mr-2" />
+        <span class="text-base">Level {{ level }}</span>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { mapState } from "vuex";
+
+export default Vue.extend({
+  computed: mapState("Challenges", ["level"]),
+});
+</script>
